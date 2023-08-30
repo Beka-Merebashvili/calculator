b2eebaf71e2629570a49c90249503f05483fbdfe
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2023-08-31 00:03:36 +0400

    create globalstyles

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Globalstyles from './styled-components/GlobalStyles'
 
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
 
   return (
     <>
-     
+     <Globalstyles />
     </>
   )
 }
---
+++ src/styled-components/GlobalStyles.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+const Globalstyles = createGlobalStyle`
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    text-decoration: none;
+    font-family: 'Space Grotesk', sans-serif;
+}
+body {
+    background: green
+}
+`
+
+export default Globalstyles
+
